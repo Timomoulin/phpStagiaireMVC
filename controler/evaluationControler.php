f21c3210fe704715b4d16e3900e4dfcab4f1bd78
T.Moulin <timomoulin@msn.com>
2021-12-17 14:52:02 +0100

    formulaire ajout d'une evaluation

--- controler/evaluationControler.php
+++ controler/evaluationControler.php
@@ -4,10 +4,12 @@ if(isset($_GET["action"])){
     $action=filter_var($_GET["action"],FILTER_SANITIZE_STRING);
 }
 require("model/evaluation.php");
+require("model/matiere.php");
 switch($action){
     case "formAjout":
         $idStagiaire=filter_var($_GET["idStagiaire"],FILTER_SANITIZE_NUMBER_INT);
-        require("");
+        $lesMatieres=fetchAllMatiere();
+        require("view/evaluation/formAjout.php");
         break;
     default :
     require("view/404.php");
---
+++ view/evaluation/formAjout.php
@@ -0,0 +1,50 @@
+<?php
+$title="Formualaire Evaluation";
+ob_start();
+?>
+<div class="container">
+  
+<h1 class="text-center">Formualire Evaluation</h1>
+<form action="" method="post" novalidate>
+    <input type="hidden" name="idStagiaire" readonly required value="<?=$idStagiaire?>">
+    <div class="col-12 col-md-8 col-lg-6 mx-auto">
+        <!-- Affichage si erreur-->
+        <?php
+        if(isset($_SESSION["error"])){
+            ?>
+            <div class="alert alert-danger">
+                <?=$_SESSION["error"]?>
+            </div>
+        <?php }
+        // On supprime le msg dans la session
+        unset($_SESSION["error"])
+        ?>
+        <div class="row my-2">
+            <label for="inputDate">Date</label>
+            <input class="form-control" name="date" id="inputdate" type="date" required min="2021-01-01" >
+        </div>
+        <div class="row my-2">
+            <label for="inputNote">Note</label>
+            <input class="form-control" name="note" id="inputNote" type="number" required step="0.01" min="0" max="20" >
+        </div>
+        <div class="row my-2">
+            <label for="inputMatiere">Matiere</label>
+           <select class="form-select" name="idMatiere" id="inputMatiere" required>
+               <option value="" disabled selected>Choisir la matiere</option>
+               <?php foreach($lesMatieres as $uneMatiere){ ?>
+                <option value="<?=$uneMatiere["idMatiere"]?>"><?=$uneMatiere["nomMatiere"]?></option>
+                <?php }?>
+           </select>
+        </div>
+
+    <div class="row my-2">
+        <button class="btn btn-primary w-25">Envoyer</button>
+    </div>
+    </div>
+</form>
+</div>
+    <!--Script de verification des formulaires-->
+    <script src="./asset/js/formVerif.js"></script>
+<?php 
+$content= ob_get_clean();
+require("view/template.php");
